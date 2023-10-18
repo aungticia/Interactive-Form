9cc2e7771e550f2c3171283e3bac2a0d8fa3fc19
Aung <104800264+aungoo3@users.noreply.github.com>
2023-10-18 19:42:42 -0400

    Finished up Form Validation and Accessibility

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ design.addEventListener('change', (e) => {
 const registerForActivities = document.querySelector('#activities');
 const costForActivities = document.querySelector('#activities-cost');
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-let total = 0; // Use let instead of const for total
+let total = 0; 
 
 registerForActivities.addEventListener('change', (e) => {
     const cost = parseInt(e.target.getAttribute('data-cost'));
@@ -82,40 +82,67 @@ const userEmail = document.querySelector('#email');
 const cardNumber = document.querySelector('#cc-num');
 const zipCode = document.querySelector('#zip');
 const cvv = document.querySelector('#cvv');
+const selectPayment = document.querySelector('#payment');
+const checkBox = document.querySelectorAll('input[type="checkbox"]');
 
 const isNameValid = (userName) => {
-
+    return /^[\S\s]+[\S]+$/i.test(userName);
 };
 
 const isEmailValid = (userEmail) => {
-
+    return /^[^@]+@[^@.]+\.[a-z]+$/i.test(userEmail);
 };
 
 const isCardNumberValid = (cardNumber) => {
-
+    if (selectPayment.value === 'credit-card') { 
+        return /^[0-9]{13,16}$/.test(cardNumber); } 
 };
 
 const isCVVValid = (cvv) => {
-
+    if (selectPayment.value === 'credit-card') { 
+        return /^[0-9]{3}$/.test(cvv); }
 };
 
 const isZipCodeValid = (zipCode) => {
-
+    if (selectPayment.value === 'credit-card') { 
+        return /^[0-9]{5}$/.test(zipCode); }
 };
 
 const isActivityValid = () => {
     return total > 0;
 };
 
+form.addEventListener('submit', (e) => { 
+    const validator = (inputElement, validationFunction) => { 
+    const inputValue = inputElement.value; 
+        if (!validationFunction(inputValue)) { 
+            e.preventDefault(); inputElement.parentElement.classList.remove('valid'); 
+            inputElement.parentElement.classList.add('error-border', 'not-valid'); 
+            inputElement.nextElementSibling.style.display = 'block'; 
+        } else { 
+            inputElement.parentElement.classList.add('valid'); 
+            inputElement.parentElement.classList.remove('error-border', 'not-valid'); 
+            inputElement.nextElementSibling.style.display = 'none'; 
+        } 
+    }; 
+        validator(userName, isNameValid); 
+        validator(userEmail, isEmailValid); 
+        validator(cardNumber, isCardNumberValid); 
+        validator(zipCode, isZipCodeValid); 
+        validator(cvv, isCVVValid); 
+        validator(costForActivities, isActivityValid); 
+}); 
 
-
-
-
-
-
-
-
-
-
-
+// Accessibility
+const checkBoxInput = document.querySelectorAll('input[type="checkbox"]');
+for (let i = 0; i < checkBoxInput.length; i++) { 
+    checkBoxInput[i].addEventListener('focus', (e) => { 
+        let parent = checkBoxInput[i].parentElement; 
+        parent.classList.add('focus') 
+    }); 
+    checkBoxInput[i].addEventListener('blur', (e) => { 
+        let parent = checkBoxInput[i].parentElement; 
+        parent.classList.remove('focus');
+    }); 
+}
 
